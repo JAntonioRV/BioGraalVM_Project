2205bdd11e5947457cce7b1ef8ebb50fda87c946
Antonio Romero <romeroventura@hotmail.com>
2020-12-16 14:31:25 -0600

    add BL to controller issue #6

--- mngraalvm/src/main/java/com/msc/mngraalvm/HomeController.java
+++ mngraalvm/src/main/java/com/msc/mngraalvm/HomeController.java
@@ -1,4 +1,4 @@
-package mngraalvm;
+package com.msc.mngraalvm;
 
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
@@ -7,8 +7,20 @@ import io.micronaut.http.HttpStatus;
 @Controller("/home")
 public class HomeController {
 
+    final private String _VERSION="v0.1";
+
     @Get("/")
     public HttpStatus index() {
         return HttpStatus.OK;
+    }
+
+    @Get("/about")
+    String command(String command) {
+        String result = "";
+
+        if(command.toUpperCase().equals("V"))
+            result = _VERSION;
+
+        return result;
     }
 }
