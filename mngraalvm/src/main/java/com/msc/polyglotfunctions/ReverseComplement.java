77e2d0d288e36f037c1c7c0d807f6c115652d20b
Antonio Romero <romeroventura@hotmail.com>
2020-12-28 21:18:20 -0600

    Adding BL code to reverse complement to solve issue #16

--- mngraalvm/src/main/java/com/msc/polyglotfunctions/ReverseComplement.java
+++ mngraalvm/src/main/java/com/msc/polyglotfunctions/ReverseComplement.java
@@ -1,8 +1,29 @@
 package com.msc.polyglotfunctions;
 
-public class ReverseComplement implements ISequence {
+import com.msc.graalvmcontext.GraalVMContext;
+import io.micronaut.core.annotation.Introspected;
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Value;
+
+@Introspected
+public class ReverseComplement extends SequenceResourses implements ISequence {
     @Override
     public String run(String sequence) {
-        return null;
+        String reversecomplement = "";
+        String pythonscript="";
+
+        //Create context instance
+        Context context = GraalVMContext.getInstance();
+        //Get python script from resources
+        pythonscript = readResource("com/msc/pythonscripts/SeqReverseComplement.py");
+        //Execute eval to load python function
+        context.eval("python",pythonscript);
+        //Execute python function setting sequence
+        Value pyfunction = context.getPolyglotBindings().getMember("reverse_complement");
+        Value sqrevcomplement = pyfunction.execute(sequence);
+        //Casting result as String
+        reversecomplement = sqrevcomplement.asString();
+
+        return reversecomplement;
     }
 }
